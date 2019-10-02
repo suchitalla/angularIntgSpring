8ce59034db8aec6112682eb0124b2bd802623bcb
suchitalla <suchitalla@yahoo.com>
2019-10-02 14:03:33 -0400

    Initial test

---
+++ src/test/java/com/insuchi/webservices/PersonControllerTest.java
@@ -0,0 +1,11 @@
+package com.insuchi.webservices;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@WebMvcTest(PersonController.class)
+@RunWith(SpringRunner.class)
+public class PersonControllerTest {
+
+}
